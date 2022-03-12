6b923c3310ca5d6cb89a2ca1a95d5f032c99a73b
Thanh Tran <thanh.trandung@ivnd.com.vn>
2022-03-12 14:59:25 +0700

    edit Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.facebook.com/ThanhTranDung1405">Thanh Tran!</a>
         </h1>
 
         <p className={styles.description}>
