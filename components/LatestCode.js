2ddf75cfea5bf7b57df450b102b53b4a9e6e1ce9
Thanh Tran <thanh.trandung@ivnd.com.vn>
2022-05-17 14:47:06 +0700

    update about me

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -13,7 +13,7 @@ export default function LatestCode({ repositories }) {
   }, []);
   return (
     <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-40">
-      <div className="max-w-6xl mx-auto">
+      {/* <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center lg:text-left">
             Latest Code
@@ -42,14 +42,14 @@ export default function LatestCode({ repositories }) {
           </a>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-10 lg:-mt-10 gap-y-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-10 lg:-mt-10 gap-y-20"> */}
         {/* Single github Repo */}
 
-        {repos &&
+        {/* {repos &&
           repos.map((latestRepo, idx) => (
             <GithubRepoCard latestRepo={latestRepo} key="idx" />
           ))}
-      </div>
+      </div> */}
     </section>
   );
 }
